54f045ca5c2075e47e7163dbcec33597a13d7713
Keven-Ferreira <kevencamelo@gmail.com>
2024-05-01 20:30:16 -0300

    Estudos do dia 01-05-2024

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,18 @@
+public class MinhaClasse {
+    
+
+    public static void main(String[] args) {
+         System.out.print("Olá turma, sejam bem vindos!\n");
+         String primeiroNome = "Keven", segundoNome = "Camêlo";
+         String meuNome = nomeCompleto(primeiroNome, segundoNome);
+         
+         String anoFabricacao = "1997";
+         System.out.print(meuNome.concat(" nasceu no ano de ").concat(anoFabricacao));
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+}
